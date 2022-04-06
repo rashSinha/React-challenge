1bbc12b0d1e7730ceaf36b06d91dfde8a2f0f983
Rashmil Sinha <rashmil.sinha@sleepnumber.com>
2022-04-05 21:41:29 -0700

    Add Spotify API

---
+++ src/api.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export const api = () => {
+  const ACCESS_TOKEN =
+    "BQA_6YS1aTsxMQiOcxk4YpESd1mgkX2bTEh-tXXB216hDFbosFXytl0sXVQwjKr0EwmuxRqD31cE0WtcURbVSyKyziv9l9Kww_hgQvr0B37SikbhojMder6E8eFUvD4NRoVVwhNoS8aN9UT39POYn3uu9IxNpgJMCS6OFuQi3CeQw5DCO2oVgA";
+
+  const REFRESH_TOKEN =
+    "AQCFuMUTPkFk2iKVuR56p13CUA0XCcD63AAs5pwX86EZ5qermBf6inHDS2ytSKX05IXDMLQQTSxhPbJN-7uWinOd4x0-S3RE-jpInQBQWUzKD3CQZD7IQaDLZ627DluKarY";
+};
+
+export default api;
