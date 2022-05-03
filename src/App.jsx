c1ebb2971cdca0fd70febad2bc11e164e4aaa8a2
rashSinha <rashmils@buffalo.edu>
2022-05-03 11:04:51 +0530

    Update App and global css to display the artist cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
-import React, { Component} from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import "./global.css"
+import Detail from "./pages/Detail";
+import Loading from "./components/Loading";
+import ErrorMessage from "./components/ErrorMessage";
+import http from "./services/http";
+import { getAccessToken } from "./services/auth";
+import "./global.css";
 
-/**
- * The starting page for your App
- */
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [authError, setAuthError] = useState(false);
 
-class App extends Component{
-  render(){
-    return(
-      <>
-        <BrowserRouter>
-          <Header />
-          <main>
-            <section>
-                <Routes>
-                  <Route path={"/"} element={<Home />} />
-                </Routes>
-            </section>
-          </main>
-        </BrowserRouter>
-      </>
+  useEffect(() => {
+    authenticate();
+  });
 
-    );
-  }
-}
+  const authenticate = async () => {
+    try {
+      const { data } = await getAccessToken();
+      http.setAuthToken(data.access_token);
+    } catch (error) {
+      setAuthError(true);
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        {loading && <Loading />}
+        {!loading && authError && (
+          <ErrorMessage message="Spotify authentication error." />
+        )}
+        {!loading && !authError && (
+          <section>
+            <Routes>
+              <Route path={"/"} element={<Home />} />
+              <Route path={"/detail/:id"} element={<Detail />} />
+            </Routes>
+          </section>
+        )}
+      </main>
+    </BrowserRouter>
+  );
+};
 
 export default App;
